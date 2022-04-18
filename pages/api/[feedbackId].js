88a4a9ade6453210a5b714dbf30fa0ffaf2fe9f8
Connor Mackay <connor.mackay@gmail.com>
2022-04-18 16:47:25 -0400

    build dynamic api route

---
+++ pages/api/[feedbackId].js
@@ -0,0 +1,11 @@
+import { buildFilePath, extractData } from '../feedback/'
+
+function handler(req, res) {
+  const feedBackId = req.query.feedbackId;
+  const filePath = buildFilePath();
+  const data = extractData(filePath);
+  const item = data.find(feedback => feedback.id === feedBackId);
+  res.status(200).json({ feedback: item });
+};
+
+export default handler;
