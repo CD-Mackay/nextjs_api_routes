bcf6fb008343e8123a81110b3a1fb548eb3d4976
Connor Mackay <connor.mackay@gmail.com>
2022-04-18 16:01:50 -0400

    add get request handling to feedback.js

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -1,6 +1,16 @@
 import fs from "fs";
 import path from "path";
 
+function buildFilePath() {
+  return path.join(process.cwd(), "data", "feedback.json");
+}
+
+function extractData(filePath) {
+  const fileData = fs.readFileSync(filePath);
+  const data = JSON.parse(fileData);
+  return data;
+}
+
 function handler(req, res) {
   if (req.method === "POST") {
     const email = req.body.email;
@@ -12,14 +22,17 @@ function handler(req, res) {
       feedback,
     };
 
-    const filePath = path.join(process.cwd(), "data", "feedback.json");
-    const fileData = fs.readFileSync(filePath);
-    const data = JSON.parse(fileData);
+    const filePath = buildFilePath();
+    const data = extractData(filePath);
     data.push(newFeedback);
     fs.writeFileSync(filePath, JSON.stringify(data));
     res
       .status(201)
       .json({ message: "heyyooo it worked!", feedback: newFeedback });
+  } else if (req.method === "GET") {
+    const filePath = buildFilePath();
+    const data = extractData(filePath);
+    res.status(201).json({ message: "success!", feedback: data });
   } else {
     res.status(200).json({
       message: "a message",
